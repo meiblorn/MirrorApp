2daf9722ee8ff01d41eb83f5dccc114343dca894
Vadim Fedorenko <meiblorn@gmail.com>
2013-07-31 19:41:35 +0400

    + int->Integer in Event.java + mv Event.hbm.xml(not working) + refactoring ajax.js + mirror issues.

--- bom/src/main/java/com/griddynamics/meiblorn/domain/Event.java
+++ bom/src/main/java/com/griddynamics/meiblorn/domain/Event.java
@@ -1,7 +1,7 @@
 package com.griddynamics.meiblorn.domain;
 
 public class Event {
-    private int id;
+    private Integer id;
     private String message;
 
     public Event() {
--- service/src/main/webapp/resources/js/ajax.js
+++ service/src/main/webapp/resources/js/ajax.js
@@ -9,20 +9,30 @@ $(document).ready(function() {
     }
 
     function addEvent(id, message) {
-        var template = $("<div>").addClass("event").
-            append($("<div>").addClass("event-header").
-                append($("<div>").addClass("event-id-header")
-                    .text(id)
-                ).
-                append($("<div>").addClass("event-link").
-                    append($('<a href="get/'+ id + '">event page&raquo;</a>'))
-                )
-                .click(function() {
-                    $(this).next("div .event-data").slideToggle(500)
-                })
-        ).
-        append($("<div>").addClass("event-data").text(message).hide());
+        var eventTmpl = $("<div>");
+            eventTmpl.addClass("event");
+            var eventHeaderTmpl = $("<div>");
+                eventHeaderTmpl.addClass("event-header");
+                var eventHeaderIdTmpl = $("<div>");
+                    eventHeaderIdTmpl.addClass("event-id-header");
+                    eventHeaderIdTmpl.text(id);
+                    eventHeaderIdTmpl.click(function() {
+                        $(this).next("div .event-data").slideToggle(500)
+                    })
+                var eventHeaderLinkTmpl = $("<div>");
+                    eventHeaderLinkTmpl.addClass("event-link");
+                    eventHeaderLinkTmpl.append($('<a href="get/'
+                        + id + '">event page&raquo;</a>'));
+            var eventDataTmpl = $("<div>");
+                eventDataTmpl.addClass("event-data");
+                eventDataTmpl.text(message);
+                eventDataTmpl.hide();
 
+        eventHeaderTmpl.append(eventHeaderIdTmpl);
+        eventHeaderTmpl.append(eventHeaderLinkTmpl);
+
+        eventTmpl.append(eventHeaderTmpl);
+        eventTmpl.append(eventDataTmpl);
         $("#events").append(template);
     }
 
