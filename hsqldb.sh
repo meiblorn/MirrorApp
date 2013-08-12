a4d549f197e8802a0c734e66d115ee21f82baad0
Vadim Fedorenko <meiblorn@gmail.com>
2013-08-12 13:53:21 +0400

    hsqldb support released

--- hsqldb.sh
+++ hsqldb.sh
@@ -6,4 +6,6 @@ DIR="$(cd "$( dirname "$0" )" && pwd)"
 # include env configuration
 . $DIR/environment.cfg
 
-java -cp $hsqldb_dir/lib/hsqldb.jar org.hsqldb.Server --props $DIR/database/db.properties
+java -cp $hsqldb_dir/lib/hsqldb.jar org.hsqldb.Server \
+    -database.0 file:database/db \
+    -dbname.0 mirrorapp
