19afe904fb39f6b919b3cf9ed19bdcd4a4b4e3b2
Vadim Fedorenko <meiblorn@gmail.com>
2013-08-12 14:12:18 +0400

    agent & deploy scripts added

---
+++ agent.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR="$(cd "$( dirname "$0" )" && pwd)"
+
+# include env configuration
+. $DIR/environment.cfg
+
+export IDE_REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8088"
+export GSC_JAVA_OPTIONS="-Duser.region=us -Duser.language=en $IDE_REMOTE_DEBUG"
+
+# Lookup group
+export LOOKUPGROUPS="MirrorApp"
+
+$gs_dir/bin/gs-agent.sh gsa.gsc 1 gsa.global.gsm 1 gsa.global.lus 1
+
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# shell scripts directory
+DIR="$(cd "$( dirname "$0" )" && pwd)"
+
+# include env configuration
+. $DIR/environment.cfg
+
+# maven package
+cd $DIR
+mvn package
+
+# Lookup group
+export LOOKUPGROUPS="MirrorApp"
+
+# deploy mirror
+$gs_dir/bin/gs.sh deploy \
+    -max-instances-per-vm 4 \
+    -max-instances-per-machine 4 \
+    -deploy-timeout 1500 \
+    -override-name mirror $DIR/mirror/target/MirrorApp-mirror.war
+
+# deploy service
+$gs_dir/bin/gs.sh deploy \
+    -cluster schema=partitioned-sync2backup total_members=1,1 \
+    -max-instances-per-vm 4 \
+    -max-instances-per-machine 4 \
+    -deploy-timeout 1500 \
+    -override-name service $DIR/service/target/MirrorApp-service.war
