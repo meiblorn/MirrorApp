50ee5d760ea6e67dd038e74a1e96bfebce512ca7
Vadim Fedorenko <meiblorn@gmail.com>
2013-08-13 13:25:08 +0400

    initial load support added

--- service/src/main/java/com/griddynamics/persistency/EventHibernateExternalDataSource.java
+++ service/src/main/java/com/griddynamics/persistency/EventHibernateExternalDataSource.java
@@ -6,17 +6,37 @@ import com.griddynamics.domain.Event;
 import com.j_spaces.core.client.SQLQuery;
 import org.openspaces.persistency.hibernate.DefaultHibernateExternalDataSource;
 import org.openspaces.persistency.hibernate.iterator.DefaultListQueryDataIterator;
+import org.openspaces.persistency.hibernate.iterator.DefaultScrollableDataIterator;
+import org.springframework.beans.factory.annotation.Required;
 
 public class EventHibernateExternalDataSource extends DefaultHibernateExternalDataSource {
+
+    private String hquery;
+
+    public String getHquery() {
+        return hquery;
+    }
+
+    @Required
+    public void setHquery(String hquery) {
+        this.hquery = hquery;
+    }
+
     @Override
     public DataIterator initialLoad() throws DataSourceException {
+
         DataIterator[] iterators = new DataIterator[1];
 
-        iterators[0] = new DefaultListQueryDataIterator(
-                new SQLQuery<Event>(Event.class, "id is NOT null"),
+        int from = -1;
+        int size = -1;
+
+        iterators[0] = new DefaultScrollableDataIterator(
+                hquery,
                 getSessionFactory(),
                 getFetchSize(),
-                getInitialLoadChunkSize());
+                from,
+                size
+        );
 
         return createInitialLoadIterator(iterators);
     }
